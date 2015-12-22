e05d945b72e2470a703cb7f599a6cf25443cf45b
Louise Lista M <Louise Lista M>
2015-12-22 09:40:44 -0430

    404

--- broderjobs/main/views.py
+++ broderjobs/main/views.py
@@ -265,6 +265,10 @@ def terminos_condiciones(request):
     return render_to_response('main/terminos-condiciones.html',
                               context_instance=RequestContext(request))
 
+def error404 (request):
+    return render_to_response('main/404.html',
+                              context_instance=RequestContext(request))
+
 
 
 
