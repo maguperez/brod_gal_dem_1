11107a232610a8e2f6c30b459469e6e594070349
joalquipe <jonathanqp71@gmail.com>
2016-01-05 14:03:36 -0430

    Cambios en empresa modal info-general para modificar la ciudad

--- broderjobs/empresa/forms.py
+++ broderjobs/empresa/forms.py
@@ -2,13 +2,17 @@
 from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout
-from models import Empresa, Representante, Sector, EvaluacionEmpresa
+from models import Empresa, Representante, Sector, EvaluacionEmpresa, Pais
 
 class InfoGeneralForm(forms.ModelForm):
+
+    ciudad_hidden = forms.CharField(widget=forms.HiddenInput())
+    pais = forms.ModelChoiceField(queryset=Pais.objects.all(), empty_label="Pais", required = False, widget=forms.Select(attrs={'class': 'full', }))
+
     class Meta:
         model = Empresa
         fields = ('nombre', 'RUC', 'quienes_somos', 'sector', 'numero_funcionarios', 'facturacion_anual', 'ano_fundacion',
-                  'pais', 'ciudad', 'website')
+                  'website')
         widget = {
             'quienessomos': forms.widgets.Textarea(),
         }
