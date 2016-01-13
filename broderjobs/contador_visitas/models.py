327a2b1ffb928de78a3d987bdee2a38b0b91d32a
Gabriela Soto <sotofgabriela@gmail.com>
2016-01-13 16:51:39 -0430

    cambios estable

---
+++ broderjobs/contador_visitas/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+from main.models import Persona, Pais, Ciudad, GradoEstudio, Universidad, Carrera, TipoPuesto, CargaHoraria, Idioma, Conocimiento
+from oportunidad.models import Oportunidad
+from main import utils
+
+items_registro = utils.estado_registro()
+class VisitasOportunidad(models.Model):
+
+    oportunidad = models.ForeignKey(Oportunidad, default=None, null=True, blank=True)
+    ip = models.CharField(max_length="100", default=None, null=True, blank=True)
+
+    usuario_creacion = models.CharField(max_length="50", default=None, null=True, blank=True)
+    fecha_creacion = models.DateField(default=None, null=True, blank=True)
+    estado =  models.CharField(choices=items_registro, max_length=1, default='A', null=True, blank=True)
+
+    def __unicode__(self):
+        return unicode('%s' % (self.oportunidad)) or u''
