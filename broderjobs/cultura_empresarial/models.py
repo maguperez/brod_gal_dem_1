ca07b34dfe88db2a9324acee2c9bd5861c92ea2a
Gabriela Soto <sotofgabriela@gmail.com>
2016-02-11 16:24:20 -0430

    agregando tablas necesarias para cultura empresarial

---
+++ broderjobs/cultura_empresarial/models.py
@@ -0,0 +1,111 @@
+from django.db import models
+from estudiante.models import Estudiante
+from empresa.models import Empresa
+from main import utils
+from datetime import date, datetime
+
+items_registro = utils.estado_registro()
+letras = []
+letras.append(('D', 'D'))
+letras.append(('I', 'I'))
+letras.append(('S', 'S'))
+letras.append(('C', 'C'))
+
+# Create your models here.
+class Perfil_Cultura(models.Model):
+
+    nombre = models.CharField(max_length="50", default=None, null=True, blank=True,  )
+    descripcion = models.TextField(default=None, null=True, blank=True,  )
+    orden = models.IntegerField(default=None, null=True, blank=True )
+
+    usuario_creacion = models.CharField(max_length="50", default=None, null=True, blank=True)
+    fecha_creacion = models.DateField(default=None, null=True, blank=True)
+    usuario_modificacion = models.CharField(max_length="50", default=None, null=True, blank=True)
+    fecha_modificacion = models.DateField(default=None, null=True, blank=True)
+    estado =  models.CharField(choices=items_registro, max_length=1, default='A', null=True, blank=True)
+
+    def __unicode__(self):
+        return unicode(self.nombre) or u''
+
+    class Meta:
+        ordering = ["orden"]
+
+class Pregunta(models.Model):
+
+    descripcion = models.CharField(max_length="50", default=None, null=True, blank=True,  )
+
+    orden = models.IntegerField(default=None, null=True, blank=True )
+    usuario_creacion = models.CharField(max_length="50", default=None, null=True, blank=True)
+    fecha_creacion = models.DateField(default=None, null=True, blank=True)
+    usuario_modificacion = models.CharField(max_length="50", default=None, null=True, blank=True)
+    fecha_modificacion = models.DateField(default=None, null=True, blank=True)
+    estado =  models.CharField(choices=items_registro, max_length=1, default='A', null=True, blank=True)
+
+    def __unicode__(self):
+        return unicode(self.descripcion) or u''
+
+    class Meta:
+        ordering = ["orden"]
+
+class Respuesta(models.Model):
+
+    pregunta = models.ForeignKey(Pregunta, default=None, null=True, blank=True)
+    descripcion = models.CharField(max_length="50", default=None, null=True, blank=True,  )
+    perfil_cultura= models.ForeignKey(Perfil_Cultura, default=None, null=True, blank=True)
+
+    orden = models.IntegerField(default=None, null=True, blank=True )
+    usuario_creacion = models.CharField(max_length="50", default=None, null=True, blank=True)
+    fecha_creacion = models.DateField(default=None, null=True, blank=True)
+    usuario_modificacion = models.CharField(max_length="50", default=None, null=True, blank=True)
+    fecha_modificacion = models.DateField(default=None, null=True, blank=True)
+    estado =  models.CharField(choices=items_registro, max_length=1, default='A', null=True, blank=True)
+
+    class Meta:
+        ordering = ["pregunta", "orden"]
+
+    def __unicode__(self):
+        return unicode(self.descripcion) or u''
+
+class CulturaMatrizDISC(models.Model):
+
+    perfil_cultura = models.ForeignKey(Perfil_Cultura, default=None, null=True, blank=True)
+    letra_d =  models.IntegerField(default=None, null=True, blank=True)
+    letra_i =  models.IntegerField(default=None, null=True, blank=True)
+    letra_s =  models.IntegerField(default=None, null=True, blank=True)
+    letra_c =  models.IntegerField(default=None, null=True, blank=True)
+    resultado = models.IntegerField(default=None, null=True, blank=True )
+
+    orden = models.IntegerField(default=None, null=True, blank=True )
+    usuario_creacion = models.CharField(max_length="50", default=None, null=True, blank=True)
+    fecha_creacion = models.DateField(default=None, null=True, blank=True)
+    usuario_modificacion = models.CharField(max_length="50", default=None, null=True, blank=True)
+    fecha_modificacion = models.DateField(default=None, null=True, blank=True)
+    estado =  models.CharField(choices=items_registro, max_length=1, default='A', null=True, blank=True)
+
+    class Meta:
+        ordering = ["letra_disc", "perfil_cultura"]
+
+    def __unicode__(self):
+        return unicode(self.perfil_cultura) or u''
+
+class empresa_cultura(models.Model):
+
+    empresa = models.ForeignKey(Empresa, default=None, null=True, blank=True)
+    porcentaje_clan = models.IntegerField(default=None, null=True, blank=True )
+    porcentaje_adhocracia = models.IntegerField(default=None, null=True, blank=True )
+    porcentaje_jerarquico = models.IntegerField(default=None, null=True, blank=True )
+    porcentaje_racional = models.IntegerField(default=None, null=True, blank=True )
+
+
+    orden = models.IntegerField(default=None, null=True, blank=True )
+    usuario_creacion = models.CharField(max_length="50", default=None, null=True, blank=True)
+    fecha_creacion = models.DateField(default=None, null=True, blank=True)
+    usuario_modificacion = models.CharField(max_length="50", default=None, null=True, blank=True)
+    fecha_modificacion = models.DateField(default=None, null=True, blank=True)
+    estado =  models.CharField(choices=items_registro, max_length=1, default='A', null=True, blank=True)
+
+    class Meta:
+        ordering = ["letra_disc", "perfil_cultura"]
+
+    def __unicode__(self):
+        return unicode(self.perfil_cultura) or u''
