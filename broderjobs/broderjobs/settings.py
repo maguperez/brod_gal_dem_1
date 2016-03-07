f91b75ccde3e7eae1b17af2e1cc152a03e6e55b7
Manuel Perez <magup2006@gmail.com>
2016-03-07 16:18:05 -0430

    cambiado allowed host en setting

--- broderjobs/broderjobs/settings.py
+++ broderjobs/broderjobs/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'f6x=__b=u%r1^czhr#-)tmh%ur*o%w(knn0!jg8l1fxu3oc44q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
